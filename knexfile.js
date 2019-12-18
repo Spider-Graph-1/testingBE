07e0e16cad0af47fddbf0d3f9ec6fb3e05f34c51
Patrick Shushereba <pshushereba@gmail.com>
2019-12-18 07:42:35 -0800

    Started setting up knexfile.

---
+++ knexfile.js
@@ -0,0 +1,62 @@
+// Update with your config settings.
+
+module.exports = {
+
+  development: {
+    client: 'pg',
+    useNullAsDefault: true,
+    connection: {
+      filename: './data/spidergraph.db3'
+    },
+    migrations: {
+      directory: './data/migrations'
+    },
+    seeds: {
+      directory: './data/seeds'
+    },
+    pool: {
+      afterCreate: (conn, done) => {
+        conn.run('PRAGMA foreign_keys = ON', done);
+      }
+    }
+  },
+
+  staging: {
+    client: 'pg',
+    useNullAsDefault: true,
+    connection: {
+      filename: './data/spidergraph.db3'
+    },
+    pool: {
+      afterCreate: (conn, done) => {
+        conn.run('PRAGMA foreign_keys = ON', done);
+      }
+    },
+    migrations: {
+      directory: './data/migrations'
+    },
+    seeds: {
+      directory: './data/seeds'
+    }
+  },
+
+  production: {
+    client: 'pg',
+    useNullAsDefault: true,
+    connection: {
+      filename: './data/spidergraph.db3'
+    },
+    pool: {
+      afterCreate: (conn, done) => {
+        conn.run('PRAGMA foreign_keys = ON', done);
+      }
+    },
+    migrations: {
+      directory: './data/migrations'
+    },
+    seeds: {
+      directory: './data/seeds'
+    }
+  }
+
+};
